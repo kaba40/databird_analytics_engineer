b2f01c957de297f2219c146716f525e607362ed5
Kaba Keita <keitakaba40@yahoo.fr>
2025-06-02 22:07:35 +0000

    fit: modifying the test on store daily turnover positivity

--- tests/total_store_daily_turnover_positive.sql
+++ tests/total_store_daily_turnover_positive.sql
@@ -2,7 +2,7 @@ SELECT
     report_date,
     store_id,
     sum(total_turnover) as total_turnover
-FROM {{ref(mrt_daily_turnover_positive)}}
+FROM {{ref("mrt_daily_turnover_analysis")}}
 GROUP BY
    report_date,
     store_id
